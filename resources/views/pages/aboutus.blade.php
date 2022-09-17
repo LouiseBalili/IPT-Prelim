ee5fa9eff6efc955a80973e84c75441f77e3d8cb
LouiseBalili <balililouisedemean@gmail.com>
2022-09-17 11:21:29 +0800

    First commit

---
+++ resources/views/pages/aboutus.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IPT Prelim | About Us</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <h1>About Us</h1>
+    <a href="{{url('/home')}}">Home </a> | <a href="{{url('/contact')}}">Contact </a> | About Us
+</body>
+</html>
